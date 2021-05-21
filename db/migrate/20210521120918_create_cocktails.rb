9ede8bf950192995e0a95a920614ec1ba6dc8621
yanis khellaf <yanis.khellaf@outlook.fr>
2021-05-21 14:11:14 +0200

    cocktails table generated

---
+++ db/migrate/20210521120918_create_cocktails.rb
@@ -0,0 +1,10 @@
+class CreateCocktails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cocktails do |t|
+      t.string :name
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
