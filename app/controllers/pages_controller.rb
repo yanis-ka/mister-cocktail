789f0b75f37c98e135ef185c8e0aeca0de116e1d
yanis khellaf <yanis.khellaf@outlook.fr>
2021-05-21 17:03:56 +0200

    seeds done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
 
   def home
+    @ingredients = Ingredient.all
+    @cocktails = Cocktail.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,25 @@
 require 'json'
 require 'open-uri'
 
+Cocktail.destroy_all
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
 ingredient = JSON.parse(user_serialized)
-#
-# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-# #   Character.create(name: 'Luke', movie: movies.first)
-# Dose.destroy_all
-# Ingredient.destroy_all
+
+Dose.destroy_all
+Ingredient.destroy_all
 
 ingredient['drinks'].each do |i|
   Ingredient.create!(name: i['strIngredient1'])
 end
+
+Cocktail.create(name: 'mojito', price: 10)
+Cocktail.create(name: 'virgin mojito', price: 11)
+Cocktail.create(name: 'sex on the beach', price: 12)
+Cocktail.create(name: 'pina colada', price: 13)
+Cocktail.create(name: 'margarita', price: 14)
+Cocktail.create(name: 'diabolo grenadine', price: 15)
+Cocktail.create(name: "Fleur d'amour", price: 16)
+Cocktail.create(name: 'Bora bora', price: 17)
+Cocktail.create(name: 'Americano', price: 18)
+Cocktail.create(name: 'Shirley temple', price: 20)
