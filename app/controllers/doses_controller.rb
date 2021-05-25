5b3b15c898a9d0bf35bae254a62bac84a5442577
yanis khellaf <yanis.khellaf@outlook.fr>
2021-05-25 10:11:14 +0200

    doses controller completed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,30 @@
 class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params["cocktail_id"])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params["cocktail_id"])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    dose = Dose.find(params[:id])
+    Dose.destroy(dose.id)
+    redirect_to cocktail_path(dose.cocktail)
+  end
+
+  private
+
+  def
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
