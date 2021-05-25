0ff8f87f585b72e220db7e0a0f1ea9c91012b0b0
yanis khellaf <yanis.khellaf@outlook.fr>
2021-05-25 09:58:54 +0200

    cocktails controller completed and doses controller generated

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,9 +10,29 @@ class CocktailsController < ApplicationController
     @dose = Dose.new
   end
 
+  def new
+    @cocktail = Cocktail.new
+  end
 
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.name.downcase!
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
 
-   private
+  def find
+    search = params['search'].capitalize
+    @cocktails = Cocktail.all
+      .joins(:ingredients)
+      .where("ingredients.name LIKE '%#{search}%'")
+    render :index, notice: search
+  end
+
+  private
 
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
