864f4f9ad9a677d09d05a461ea66ac969ae7954b
yanis khellaf <yanis.khellaf@outlook.fr>
2021-05-25 17:07:22 +0200

    all forms and views done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,12 @@
 class DosesController < ApplicationController
 
   def new
-    @cocktail = Cocktail.find(params["cocktail_id"])
+    @cocktail = Cocktail.find(params['cocktail_id'])
     @dose = Dose.new
   end
 
   def create
-    @cocktail = Cocktail.find(params["cocktail_id"])
+    @cocktail = Cocktail.find(params['cocktail_id'])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
